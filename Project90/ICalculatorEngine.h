21c55d9b4adb944fc93ccd992c9ebabb8d6452c5
Carla232 <79741526+Carla232@users.noreply.github.com>
2024-02-18 21:09:25 +0200

    Delete Project90 directory

--- Project90/ICalculatorEngine.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef ICALCULATORENGINE_H
-#define ICALCULATORENGINE_H
-#include <string>
-class ICalculatorEngine {
-public:
-    virtual double calculate(const std::string& expression) = 0; 
-    virtual ~ICalculatorEngine() {} 
-};
-#endif 
